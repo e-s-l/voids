a14d2b7bbe5ca7ee0d13b9cd6e423b54ace65b8d
p3arl <p3ear95@protonmail.com>
2024-03-29 22:29:10 +0100

    ok, had some double ups in the declarations thx to the new parameter approach, now this version at least produces some data (not necessarily correct data tho)

--- twofluidlrsevolution.f90
+++ twofluidlrsevolution.f90
@@ -4,8 +4,8 @@ MODULE program_parameters
 
     integer, parameter :: nx = 12       !number of variables in the system of equations
     !SPACE & TIME DOMAIN RESOLUTION:
-    integer, parameter :: nr = 20  !number of radial grid points
-    integer, parameter :: nt = 100 !number of time grid points, might make this variable later. 
+    integer, parameter :: nr = 200  !number of radial grid points
+    integer, parameter :: nt = 500 !number of time grid points, might make this variable later. 
     !Cosmological Model Parameters:
     double precision, parameter :: omega_matter = 0.3
     double precision, parameter :: omega_lambda = 1.0 - omega_matter
@@ -18,17 +18,18 @@ MODULE program_parameters
     double precision, parameter :: lu = 3.085678d19 !length in kpc
     double precision, parameter :: tu = 31557600*1d6 ! time in 10^6 years
     !other constants (non-dimensionalised)
-    double precision, parameter :: gcons = 6.6742d-11*((mu*(tu**2))/(lu**3)) 
+    double precision, parameter :: gcons = (6.6742d-11)*((mu*(tu**2))/(lu**3)) 
     double precision, parameter :: cms = 299792458 !light-speed in m/s
     double precision, parameter :: cs = cms*(tu/lu)
     double precision, parameter :: kap = 8d0*pi*gcons*(1d0/(cs**4))
     double precision, parameter :: kapc2 = 8d0*pi*gcons*(1d0/(cs**2))
     double precision, parameter :: Ho = (tu/(lu))*H0
-    double precision, parameter :: gkr = kapc2*3d0*(((Ho)**2)/(8d0*pi*gcons))*omega_matter
+    double precision, parameter :: gkr = kapc2*omega_matter*(3d0*(((Ho)**2)/(8d0*pi*gcons)))
+
     double precision, parameter :: lb = 3d0*omega_lambda*(((Ho)**2)/(cs*cs))
     !
-    double precision, parameter :: zf = 150d0
-    double precision, parameter :: zi = 0.2d0   !final redshift/position of void-lens
+    double precision, parameter :: zi = 150d0
+    double precision, parameter :: zf = 0.2d0   !final redshift/position of void-lens
 
     ! Hamaus profile:
     double precision, parameter :: a = 2.1          !inner slope
@@ -51,9 +52,9 @@ MODULE program_parameters
 
     double precision :: ti 
     double precision :: tf 
+    double precision :: dt
 
     !NOTE: the subroutines could be contained in this module rather than the main program as below...
-
 
 END MODULE program_parameters
 
@@ -82,12 +83,33 @@ PROGRAM TwoFluidLRSEvolution
     logical, parameter ::  lrs_evolution_mode = .false. 
     logical, parameter ::  ltb_evolution_mode = .true. 
 
+    !debug:
+    logical, parameter ::  write_data = .true. 
+
     !!!!!!!!!!!!!!!!!!!
     ! INITIALISATIONS:
     !!!!!!!!!!!!!!!!!!!
 
+
+
+    call timelcdm(zi, ti)
+    call timelcdm(zf, tf)
+
+    dt = (tf - ti) / (1.0*nt)
+
+  !  print *, 'dt = ', dt
+
+
+
     call initial(X)
-    call write_X_to_file(counter,X)
+
+  !  print *, "gkr, lb = ", gkr, lb
+    print *, X(:,1)
+    !
+    if (write_data) then
+        call write_rho_to_file(counter, X)
+        call write_X_to_file(counter, X)
+    endif
 
     !!!!!!!!!!!
     !EVOLUTION
@@ -97,12 +119,13 @@ PROGRAM TwoFluidLRSEvolution
     else if (ltb_evolution_mode) then
         call ltb_evolution(X,R_ltb)
     else
-    print *,':('
+        print *,':('
     endif
 
-    call write_X_to_file(counter,X)
-
-
+    if (write_data) then
+        call write_rho_to_file(counter, X)
+        call write_X_to_file(counter, X)
+    endif
 
 
 
@@ -152,12 +175,19 @@ PROGRAM TwoFluidLRSEvolution
 
         subroutine timelcdm(zo, ctt) 
             implicit none
-            double precision zo,ct,lb,rhb,rhzo,x,arsinh,tzo,ti,ctt
+
+            double precision zo,rhb,rhzo,x,arsinh,tzo,ctt
             double precision ztt,thb
-            rhzo = gkr*( (1.0d0+zo)**3 )
+
+       !     print *, "gkr, lb = ", gkr, lb
+
+            rhzo = gkr*((1.0d0+zo)**3 )
             x = dsqrt(lb/rhzo)
             arsinh = dlog(x + dsqrt(x*x + 1d0))
             ctt = (dsqrt((4d0)/(3d0*lb)))*arsinh
+
+        !    print *, "z,t = ", zo, ctt
+
         end subroutine timelcdm
 
         !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
@@ -165,26 +195,17 @@ PROGRAM TwoFluidLRSEvolution
         subroutine ltb_shell_evolution(r,m,mr,k,kr,re,rre)
             implicit none
 
-            integer :: I, Ne
+            integer :: I
             double precision :: m,mr,k,kr
             double precision :: re,rre,ri, rri
             double precision :: r,rt,rtr,lb
             double precision :: rk1,rk2,rk3,rk4
             double precision :: rr1,rr2,rr3,rr4
-            double precision :: dt, ti, tf
 
-
-            Ne = 50
-
-
-            call timelcdm(zi, ti)
-            call timelcdm(zf, tf)
-
-            dt = (tf - ti) / (1.0*Ne)
             ri = r
             rri = 1.0d0
-
-            do I=1,Ne
+           
+            do I=1,nt
                 re = ri
                 rt = dsqrt(2.0d0*(m/re) - k + (lb/3.0d0)*re*re)
                 rk1 = dt*rt
@@ -238,29 +259,41 @@ PROGRAM TwoFluidLRSEvolution
 
         !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
+        subroutine write_rho_to_file(counter, X)
+            implicit none
+
+            character(len=50) filename
+            integer, intent(inout) :: counter
+            double precision, dimension(40) :: cpar
+            double precision, dimension(nr,nx), intent(inout) :: X
+
+            write(filename, '(A,:,(I1), A)') 'rho.lrs.',counter,'.dat' 
+            open(777, file=filename, status="replace")
+            write(777,*) X(:,1)
+
+          !  counter = counter+1
+
+        end subroutine
+
+        !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
         subroutine initial(X)
             implicit none
 
             double precision, dimension(nr,nx), intent(inout) :: X
             double precision  :: rho,tht,shr,wey
-            double precision :: r, dr, zo, zz, zf, tevo, ctf, cto
+            double precision :: r, zo, zz, tevo, ctf, cto
             integer :: I
 
             ! initial values: redshift, time instant, density, and expansion rate (the LCDM model assumed)
        
-            call timelcdm(zi,cto)
-   
-            call timelcdm(zf,ctf)
-            tevo = ctf-cto
-           
-            ! cpar(23) = tevo
-            ! cpar(24) = tevo/(1.0d0*nt)
-            ! cpar(18) = cpar(5)*(zz**3)
+         !   call timelcdm(zi,cto)
+         !   call timelcdm(zf,ctf)
+         !   tevo = ctf-cto
 
             !
             do I = 1,nr
                 r = dr*I
-
                 call ltb_profile(r,rho,tht,shr,wey)
                 X(I,:) = [rho,0.0d0,tht,shr,wey,1.0d0,0.0d0,0.0d0,0.0d0,0.0d0,0.0d0,0.0d0]
 
@@ -275,31 +308,28 @@ PROGRAM TwoFluidLRSEvolution
         subroutine ltb_profile(r,rho,tht,shr,wey)
             implicit none
 
-
-            double precision cpar(40)
-            double precision Am,Ak,r0,dlr,sgm,amp,ho,Om,ran
+            double precision Am,Ak,dlr,sgm,amp,ho,Om,ran
             double precision dta0, dta1, dta2
             double precision d0el,d1el,d2el
             double precision m,mr,mrr,m2a,m2b,m0,m1,m2,m0r
             double precision k0,k,kr,krr,cti,ke
-            double precision r,rr,rt,rtr,lb,ct
-            double precision rho,tht,shr,wey,vol,gkr
-            double precision rp,r1,r2,dr,k1,k2
+            double precision r,rr,rt,rtr
+            double precision rho,tht,shr,wey,vol
+
+            double precision rp,r1,r2,k1,k2
 
             !current implmentation for compensated profile only
 
+            Om = omega_matter
+            amp = delta0
+            ho = Ho/cs
+            dlr = sigma0*r0
 
-            gkr = cpar(18)
-            lb = cpar(7)
-            cti = cpar(21)
-            Om = cpar(3)
-            amp = cpar(17)
-            ho = cpar(2)
-            r0 = cpar(16)
-            dlr = cpar(14)*r0
+            ! cpar(18) = gkr*((zi+1.0d0)**3)
 
+          !  print *,'r =', r
             
-            Am = (1.0d0/6.0d0)*cpar(18)
+            Am = (1.0d0/6.0d0)*gkr*((zi+1.0d0)**3)
             Ak = (10.0d0/3.0d0)*Am
 
             dta0 = dtanh((r-r0)/(2.0d0*dlr))
@@ -319,13 +349,18 @@ PROGRAM TwoFluidLRSEvolution
             krr = Ak*d2el*r*r+2.0d0*Ak*d1el*r+2.0d0*kr/r-2.0d0*(k/(r*r))
 
 
-            r = r	
             rr = 1.0
             rt = dsqrt(2.0d0*(m/r) - k + (lb/3.0d0)*r*r)
             rtr = ((mr/r) - (m/(r**2))*rr - 0.5d0*kr + (lb/3d0)*r*rr)/rt
 
             !- density
             rho = 2d0*(mr)/(r*r*(rr))
+
+
+          !  print *,'dlr =', dlr
+          !  print *,'dta1 =', dta1
+          !  print *,'d1el =', d1el
+
             !- expansion
             tht =  (rtr + 2.0*rt*(rr/r))/(rr )
             !- shear
